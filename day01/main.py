5b129335661a8608371492b4af264b7ef5f74b70
Kyle <k89kirby@gmail.com>
2025-07-22 15:27:02 -0500

    Remove Day 1 folder to re-add with correct structure

--- day01/main.py
+++
@@ -1,7 +0,0 @@
-# This code takes two inputs from prompts and outputs a band name
-print("Welcome to the Band Name Generator.")
-
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-
-print("Your band name could be " + city + " " + pet)
