0009cb69a9650b800247796ce22390a2df4a51fc
Kyle <k89kirby@gmail.com>
2025-09-22 10:29:37 -0500

    Updating Snake and starting Day 24
    Working with files to save high score on snake

--- day20/scoreboard.py
+++ day20/scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -12,11 +13,17 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score : {self.score} ", align="center", font=("Arial", 22, "normal"))
+        self.write(f"Score : {self.score} High Score: {self.high_score}", align="center", font=("Arial", 22, "normal"))
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font=("Arial", 22, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align="center", font=("Arial", 22, "normal"))
 
     def increase_score(self):
         self.score += 1
