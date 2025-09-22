94bb436f9ff287e1bef1ec98e9ec7c9c57572ca2
Kyle <k89kirby@gmail.com>
2025-09-22 11:57:51 -0500

    Update Day 21: Snake
    Added a persistent high-score using a file

--- day21/scoreboard.py
+++ day21/scoreboard.py
@@ -4,7 +4,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as self.high_score:
+            self.high_score = int(self.high_score.read())
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -15,9 +16,12 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score : {self.score} High Score: {self.high_score} ", align="center", font=("Arial", 22, "normal"))
 
+
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as new_score:
+                new_score.write(str(self.high_score))
         self.score = 0
         self.update_score()
 
